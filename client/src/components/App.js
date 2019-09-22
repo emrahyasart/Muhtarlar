f367b63f2a7a5423b5561eec85947107e290ded0
emrahyasart <emrah35@gmail.com>
2019-09-22 12:05:43 +0300

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return <div className="container-fluid">App</div>;
+};
+
+export default App;
