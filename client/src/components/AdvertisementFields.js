5f1a6dae81a1aa7753243c14595294270e5a050a
emrahyasart <emrah35@gmail.com>
2019-09-23 14:34:35 +0300

    Advertisement Fields Added

---
+++ client/src/components/AdvertisementFields.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const AdvertisementFields = () => {
+  return (
+    <div>
+      <Container
+        style={{
+          margin: "100px 0px 0px 100px",
+          width: "80%",
+          height: "300px",
+          borderStyle: "solid",
+          borderWidth: "2px",
+          borderColor: "black",
+          textAlign: "center",
+          fontSize: "30px"
+        }}
+      >
+        Advertisement Field
+      </Container>
+      <Container
+        style={{
+          margin: "100px 0px 0px 100px",
+          width: "80%",
+          height: "300px",
+          borderStyle: "solid",
+          borderWidth: "2px",
+          borderColor: "black",
+          textAlign: "center",
+          fontSize: "30px"
+        }}
+      >
+        Advertisement Field
+      </Container>
+      <Container
+        style={{
+          margin: "100px 0px 0px 100px",
+          width: "80%",
+          height: "300px",
+          borderStyle: "solid",
+          borderWidth: "2px",
+          borderColor: "black",
+          textAlign: "center",
+          fontSize: "30px"
+        }}
+      >
+        Advertisement Field
+      </Container>
+    </div>
+  );
+};
+
+export default AdvertisementFields;
