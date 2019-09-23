9d8e25ea0dad2763aff4251eaed28bcb9e890b0a
emrahyasart <emrah35@gmail.com>
2019-09-23 14:18:42 +0300

    Navbar added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Navbar from "./Navbar";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div className="container-fluid">App</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-2"></div>
+        <div className="col-sm-8">
+          <Navbar />
+        </div>
+        <div className="col-sm-2"></div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
